46bef30557738db35281ace90c68e19813fc4b65
otueteng <otuofemeteng@gmail.com>
2022-12-16 01:33:48 -0800

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,16 @@
  *@c: tested character
  *Return: return 1 if it is uppercase, 0 if not
  */
-int _isdigit(int c)
-{
-	if ((c >= 48) && (c <= 57))
-	return (1);
 
+int _isdigit(int c)
+
+{
+	if ((c >= 48) && (c <= 58))
+	{
+	return (1);
+	}
+	else
+	{
 	return (0);
+	}
 }                           
