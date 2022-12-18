8a396d78074df88647f0e42edd620c9b355ffe56
otueteng <otuofemeteng@gmail.com>
2022-12-17 16:27:03 -0800

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,27 +2,27 @@
 
 /**
  * main -> Entry point
- * Return: something
+ * Return: Always 0 success
  */
 
 int main(void)
 {
-	long int i, j, k, l;
+	long int i;
+	long int fibonacci[50];
 
-	j = 1;
-	k = 2;
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 2; i < 50; i++)
 	{
-		if (j != 20365011074)
-		{
-			printf("%ld, ", j);
-		}
-
-		l = j + k;
-		j = k;
-		k = l;
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 49)
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
 	}
 	return (0);
 }
+
 
