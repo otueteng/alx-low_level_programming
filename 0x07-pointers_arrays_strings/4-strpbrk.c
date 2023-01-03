952d373d8ae1f49f02ecb1d431d0ef3f8f5fbdf8
otueteng <otuofemeteng@gmail.com>
2023-01-02 19:05:33 -0800

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,8 +5,7 @@
  * _strpbrk - Search a string for any of a set of bytes.
  * @s: string
  * @accept: string to match
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
+ * Return: pointer to index of string at first occurence
  */
 
 char *_strpbrk(char *s, char *accept)
