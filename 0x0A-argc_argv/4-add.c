d219f5b4e182d50cf80cf3689e3e438ec30d57ff
otueteng <otuofemeteng@gmail.com>
2023-01-06 14:20:42 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,81 +2,31 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * main - Prints the addition of positive numbers,
+ * 	followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ * 	Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int num, digit, sum = 0;
 
-	sum = 0;
-
-	for (i = 1; i < argc; i++)
+	for (num = 1; num < argc; num++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-				puts("Error");
-			return (1);
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[num]);
 	}
-}
 
-for (k = 1; k < argc; k++)
-{
-	num = _atoi(argv[k]);
-	if (num >= 0)
-	{
-		sum += num;
-	}
-}
-
-printf("%d\n", sum);
-return (0);
+	printf("%d\n", sum);
+	return (0);
 }
